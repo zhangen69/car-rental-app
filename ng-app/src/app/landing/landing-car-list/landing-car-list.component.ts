f0e3666f00d2f88fdfcb21fc8e7f1ae32c5d3dbb
Jacob <zhangen69@gmail.com>
2019-07-18 20:18:30 +0800

    responsive cols

--- ng-app/src/app/landing/landing-car-list/landing-car-list.component.ts
+++ ng-app/src/app/landing/landing-car-list/landing-car-list.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-car-list.component.sass']
 })
 export class LandingCarListComponent implements OnInit {
-
+  cols = 4;
   constructor() { }
 
   ngOnInit() {
+    this.onResize();
+  }
+
+  onResize() {
+    if (window.innerWidth <= 425) {
+      this.cols = 1;
+    } else if (window.innerWidth <= 768) {
+      this.cols = 2;
+    } else if (window.innerWidth <= 1024) {
+      this.cols = 3;
+    } else {
+      this.cols = 4;
+    }
   }
 
 }
