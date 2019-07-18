259b413556bb2da382ef04e4d5f21e0144eb1fb4
Jacob <zhangen69@gmail.com>
2019-07-18 21:01:46 +0800

    -add booking form

--- ng-app/src/app/booking/booking-form/booking-form.component.ts
+++ ng-app/src/app/booking/booking-form/booking-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-booking-form',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking-form.component.sass']
 })
 export class BookingFormComponent implements OnInit {
+  formData = this.formBuilder.group({
+    model: ['', Validators.required],
+    brand: ['', Validators.required],
+    carNumber: ['', Validators.required],
+    color: ['', Validators.required],
+    photo: ['', Validators.required],
+    insurance: ['', Validators.required],
+    roadTax: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(formData) {
+    let bookings = JSON.parse(localStorage.getItem('bookings'));
+
+    if (bookings === null) {
+      bookings = [];
+    }
+
+    const data = { ...formData };
+    data.id = uuid.v4();
+
+    bookings.push(data);
+
+    localStorage.setItem('bookings', JSON.stringify(bookings));
+
   }
 
 }
